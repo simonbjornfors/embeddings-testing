f60b9f7d68b2603b8e42fdfd8981ab5d08b1c51d
simonbjornfors <simonbjornfors@protonmail.com>
2023-10-02 13:41:32 +0200

    layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
   import "../app.postcss";
 </script>
-  <nav class="navbar bg-primary text-primary-content px-6">
-    <div class="navbar-start text-2xl font-bold">
-      <a href="/">Embeddings</a>
+  <nav class="navbar bg-primary text-primary-content w-screen">
+    <div class="navbar-start">
+      <a class="btn btn-ghost text-2xl font-bold" href="/">Embeddings</a>
     </div>
     <div class="navbar-center">
     </div>
-    <div class="navbar-end">
+    <div class="navbar-end mr-3">
       <ul class="menu menu-horizontal"> 
         <li><a class="btn btn-ghost" href="/">Speed Test</a></li>
         <li><a class="btn btn-ghost" href="/text-comparison">Text Comparison</a></li>
